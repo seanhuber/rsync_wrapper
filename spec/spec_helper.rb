d3db1d72d6c3bb56642a09015bd7419a68c2ff7a
Sean Huber <seanhuber@seanhuber.com>
2018-03-08 10:26:54 -0800

    coveralls not checking all source lines

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'rsync_wrapper'
 require 'coveralls'
 Coveralls.wear!
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'rsync_wrapper'
